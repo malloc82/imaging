98f98975a850af0afc700013b1b655892219ce09
Ritchie Cai <ritchiecai@r2labs.org>
2015-06-10 22:40:02 -0500

    load-txt-image update using mostly persistent vector, better performance than double-array

--- src/imaging/dicom.clj
+++ src/imaging/dicom.clj
@@ -3,7 +3,8 @@
         clojure.java.io
         [clojure.core.matrix :as mat]
         [clojure.pprint :only [pprint]])
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
   (:import [java.io File FileInputStream DataInputStream]
            [ij IJ]
            ij.plugin.DICOM
@@ -13,6 +14,7 @@
            [javax.swing JScrollPane ImageIcon]
            java.awt.BorderLayout))
 
+(def test_samples "resources/PCT/CTP404_merged")
 
 (defn dicomread [filename]
   (let [img (IJ/openImage filename)
@@ -25,44 +27,73 @@
         info (.getProperty img "Info")]
     (apply hash-map (map #(str/trim %) (str/split info #"[\n:]")))))
 
-(defn txt2dicom [src_fname]
+(def dicom_meta_list (keys (dicominfo "resources/dicom_template.dcm")))
+
+(def dicom_meta_map  (reduce
+                      (fn [s [k v]]
+                        (conj s {k v}))
+                      {}
+                      (map #(list (subs % 0 9) (subs % 11))
+                           (keys (dicominfo "resources/dicom_template.dcm")))))
+
+(defn min-max-by-columns [s]
+  (reduce (fn [[smallest largest] y]
+            [(min smallest y) (max largest y)]
+            [(doall (map min smallest y)) (doall (map max largest y))])
+          [(first s) (first s)]
+          s))
+
+(defn load-txt-image [src_fname & {:keys [show debug]
+                                   :or   {show  false
+                                          debug false}}]
   (let [[row col] (with-open [rdr (reader src_fname)]
                     (let [stream (line-seq rdr)]
                       [(count stream)
                        (count (str/split (first (take 1 stream)) #"\s"))]))
-        v_data    (str/split (IJ/openAsString src_fname) #"[\s]")
-        s_data    (double-array (* row col))
+        data      (timer "loading txt data"
+                         (map #(Double. %) (str/split (slurp "resources/PCT/CTP404_merged/x_10.txt") #"[\s]+")))
         image     (BufferedImage. col row BufferedImage/TYPE_USHORT_GRAY)]
-    (println "row =" row "  col =" col)
-    (doseq [r (range row)
-            c (range col)]
-      (let [idx (+ (* r col) c)]
-        (aset s_data idx (Double. (v_data idx)))))
-    (.setPixels ^WritableRaster (.getRaster image) 0 0 col row s_data)
-    (let [canvas (JLabel. (ImageIcon. image))]
-      (doto (JFrame.)
-        (.setSize (Dimension. col (* col 2)))
-        ;; (-> .getContentPane (.add (doto (JPanel.)
-        ;;                             (.setLayout (BorderLayout.))
-        ;;                             (.add (doto (JScrollPane.)
-        ;;                                     (-> .getViewport (.add canvas))
-        ;;                                     (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
-        ;;                                     (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))
-        ;;                                   BorderLayout/CENTER))))
-        (-> .getContentPane (.add (doto (JScrollPane.)
-                                    (-> .getViewport (.add canvas))
-                                    (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
-                                    (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))))
-        (.setVisible true)
-        (.show))
-      (println (.getSize canvas))
-      canvas)))
+    (when debug
+      (println "row =" row "  col =" col " count =" (count data) (class data)))
+    (let [max_val (timer "max" (apply max data))
+          min_val (timer "min" (apply min data))
+          ;; [min_val max_val] (timer "min max : " (reduce (fn [[_min _max] elem]
+          ;;                                                 [(if (< elem _min) elem _min)
+          ;;                                                  (if (> elem _max) elem _max)])
+          ;;                                               [0.0 0.0]
+          ;;                                               data))
+          ]
+      (when debug
+        (println "min =" min_val " max =" max_val))
+      (timer "update pixel values"
+             (let [scaled_range (- max_val min_val)]
+               (.setPixels ^WritableRaster (.getRaster image) 0 0 col row
+                           (double-array (map #(* (/ (- % min_val) scaled_range) 0xffff) data))))))
+    (timer "displaying"
+           (let [canvas (JLabel. (ImageIcon. image))]
+             (doto (JFrame.)
+               (.setSize (Dimension. (+ col 40) (* col 5)))
+               ;; (-> .getContentPane
+               ;;     (.add (doto (JPanel.)
+               ;;             (.setLayout (BorderLayout.))
+               ;;             (.add (doto (JScrollPane.)
+               ;;                     (-> .getViewport (.add canvas))
+               ;;                     (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
+               ;;                     (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))
+               ;;                   BorderLayout/CENTER))))
+               (-> .getContentPane (.add (doto (JScrollPane.)
+                                           (-> .getViewport (.add canvas))
+                                           (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
+                                           (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))))
+               (.setVisible true)
+               (.show))))
+    data))
 
-(defmacro timer [call]
-  `(let [startTime# (System/nanoTime)
-         retval#    ~call]
-     (println (/ (- (System/nanoTime) startTime#) 1000000.0) "ms")
-     retval#))
+(defmacro timer [msg call]
+  `(let [startTime# (System/nanoTime)]
+     (let [retval#    ~call]
+       (println "  ==> timing:  " ~msg " " (/ (- (System/nanoTime) startTime#) 1000000.0) "ms")
+       retval#)))
 
 ;; (doto (ij.io.FileInfo.)
 ;;   (-> .fileName (set! "x_0.txt"))
