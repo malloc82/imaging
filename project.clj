bf5744c97652c214d67397a3f52cfa5cdeb4ca68
Ritchie Cai <ritchiecai@r2labs.org>
2015-06-25 12:25:23 -0500

    started using dcm4che for dicom processing. code incomplete

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  ;; :repositories [["org.slf4j" "http://mvnrepository.com/artifact/org.slf4j"]]
+  :dependencies [[org.clojure/clojure    "1.7.0-RC2"]
                  [net.mikera/core.matrix "0.34.0"]
-                 [net.mikera/vectorz-clj "0.29.0"]]
-  :resource-paths ["resources/ij.jar"])
+                 [clatrix "0.5.0"]
+                 [net.mikera/vectorz     "0.47.1-SNAPSHOT"]
+                 [net.mikera/vectorz-clj "0.29.0"]
+                 [org.slf4j/slf4j-api    "1.7.12"]]
+  :jvm-opts ^:replace []
+  :resource-paths ["lib/ij.jar"
+                   "lib/dcm4che-core-3.3.7.jar"])
--- src/imaging/dicom.clj
+++ src/imaging/dicom.clj
@@ -1,18 +1,35 @@
 (ns imaging.dicom
   (:use clojure.core
-        clojure.java.io
         [clojure.core.matrix :as mat]
         [clojure.pprint :only [pprint]])
   (:require [clojure.string :as str]
-            [clojure.java.io :as io])
-  (:import [java.io File FileInputStream DataInputStream]
+            [clojure.java.io :as io]
+            [clojure.core.matrix.operators :as op])
+  (:import [mikera.matrixx Matrix]
            [ij IJ]
            ij.plugin.DICOM
+           ij.io.ImageWriter
+
+           [org.dcm4che3.io DicomInputStream DicomOutputStream]
+           [org.dcm4che3.io.DicomInputStream.IncludeBulkData]
+           [org.dcm4che3.data Attributes Tag UID VR]
+           [org.dcm4che3.util UIDUtils]
+
+           [java.io File InputStream FileInputStream FileOutputStream DataInputStream]
+           java.io.InputStream
            (java.awt.image BufferedImage Raster WritableRaster)
            (javax.swing JFrame JLabel JPanel)
            (java.awt Graphics Dimension Color)
            [javax.swing JScrollPane ImageIcon]
            java.awt.BorderLayout))
+
+(mat/set-current-implementation :vectorz)
+
+(defmacro timer [msg call]
+  `(let [startTime# (System/nanoTime)]
+     (let [retval#    ~call]
+       (println "  ==> timing:  " ~msg " " (/ (- (System/nanoTime) startTime#) 1000000.0) "ms")
+       retval#)))
 
 (def test_samples "resources/PCT/CTP404_merged")
 
@@ -36,69 +53,92 @@
                       (map #(list (subs % 0 9) (subs % 11))
                            (keys (dicominfo "resources/dicom_template.dcm")))))
 
-(defn min-max-by-columns [s]
-  (reduce (fn [[smallest largest] y]
-            [(min smallest y) (max largest y)]
-            [(doall (map min smallest y)) (doall (map max largest y))])
-          [(first s) (first s)]
-          s))
+(defn imshow [^Matrix data]
+  (let [[row col] (mat/shape data)
+        image     ^BufferedImage (BufferedImage. ^long col ^long row BufferedImage/TYPE_USHORT_GRAY)
+        max_val   ^double (timer "emax" (mat/emax data))
+        min_val   ^double (timer "emin" (mat/emin data))
+        scaled_range ^double (double (- max_val min_val))]
+    (timer "update pixel values"
+           (.setPixels ^WritableRaster (.getRaster ^BufferedImage image) 0 0 ^long col ^long row
+                       ^doubles (mat/to-double-array (op/* (op// (op/- data min_val) scaled_range) 0xffff))))
+    (let [canvas (JLabel. (ImageIcon. image))]
+      (doto (JFrame.)
+        (.setSize (Dimension. (+ col 50) (+ row 50)))
+        (-> .getContentPane
+            (.add (doto (JScrollPane.)
+                    (-> .getViewport (.add canvas))
+                    (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
+                    (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))))
+        (.setVisible true)
+        (.show)))))
 
-(defn load-txt-image [src_fname & {:keys [show debug]
+(defn load-txt-data [src_fname & {:keys [show debug]
                                    :or   {show  false
                                           debug false}}]
-  (let [[row col] (with-open [rdr (reader src_fname)]
+  (let [[row col] (with-open [rdr (io/reader src_fname)]
                     (let [stream (line-seq rdr)]
                       [(count stream)
                        (count (str/split (first (take 1 stream)) #"\s"))]))
         data      (timer "loading txt data"
-                         (map #(Double. %) (str/split (slurp "resources/PCT/CTP404_merged/x_10.txt") #"[\s]+")))
-        image     (BufferedImage. col row BufferedImage/TYPE_USHORT_GRAY)]
+                         (Matrix/wrap row col
+                                      (double-array (mapv #(Double. ^String %)
+                                                          (str/split (slurp src_fname) #"[\s]+")))))]
+    (when show
+      (timer "displaying" (imshow data)))
+    data))
+
+(defn load-txt-image [src_fname & {:keys [show debug]
+                                   :or   {show  false
+                                          debug false}}]
+  (let [[row col] (with-open [rdr (io/reader src_fname)]
+                    (let [stream (line-seq rdr)]
+                      [(count stream)
+                       (count (str/split (first (take 1 stream)) #"\s"))]))
+        data      (timer "loading txt data"
+                         (Matrix/wrap row col
+                                      (double-array (mapv #(Double. ^String %)
+                                                          (str/split (slurp src_fname) #"[\s]+")))))
+        image     (BufferedImage. ^long col ^long row BufferedImage/TYPE_USHORT_GRAY)]
     (when debug
       (println "row =" row "  col =" col " count =" (count data) (class data)))
-    (let [max_val (timer "max" (apply max data))
-          min_val (timer "min" (apply min data))
-          ;; [min_val max_val] (timer "min max : " (reduce (fn [[_min _max] elem]
-          ;;                                                 [(if (< elem _min) elem _min)
-          ;;                                                  (if (> elem _max) elem _max)])
-          ;;                                               [0.0 0.0]
-          ;;                                               data))
-          ]
+    (let [max_val (timer "max" (mat/emax data))
+          min_val (timer "min" (mat/emin data))
+          scaled_range ^double  (- max_val min_val)]
       (when debug
         (println "min =" min_val " max =" max_val))
       (timer "update pixel values"
-             (let [scaled_range (- max_val min_val)]
-               (.setPixels ^WritableRaster (.getRaster image) 0 0 col row
-                           (double-array (map #(* (/ (- % min_val) scaled_range) 0xffff) data))))))
-    (timer "displaying"
-           (let [canvas (JLabel. (ImageIcon. image))]
-             (doto (JFrame.)
-               (.setSize (Dimension. (+ col 40) (* col 5)))
-               ;; (-> .getContentPane
-               ;;     (.add (doto (JPanel.)
-               ;;             (.setLayout (BorderLayout.))
-               ;;             (.add (doto (JScrollPane.)
-               ;;                     (-> .getViewport (.add canvas))
-               ;;                     (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
-               ;;                     (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))
-               ;;                   BorderLayout/CENTER))))
-               (-> .getContentPane (.add (doto (JScrollPane.)
-                                           (-> .getViewport (.add canvas))
-                                           (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
-                                           (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))))
-               (.setVisible true)
-               (.show))))
-    data))
+             (.setPixels ^WritableRaster (.getRaster image) 0 0 ^long col ^long row
+                         ^doubles (mat/to-double-array (op/* (op// (op/- data min_val) scaled_range) 0xffff))))
+      (when show
+        (timer "displaying"
+               (let [canvas (JLabel. (ImageIcon. image))]
+                 (doto (JFrame.)
+                   (.setSize (Dimension. (+ col 40) (* col 5)))
+                   (-> .getContentPane
+                       (.add (doto (JScrollPane.)
+                               (-> .getViewport (.add canvas))
+                               (.setHorizontalScrollBarPolicy JScrollPane/HORIZONTAL_SCROLLBAR_AS_NEEDED)
+                               (.setVerticalScrollBarPolicy JScrollPane/VERTICAL_SCROLLBAR_AS_NEEDED))))
+                   (.setVisible true)
+                   (.show)))))
+      (mat/to-double-array data))))
 
-(defmacro timer [msg call]
-  `(let [startTime# (System/nanoTime)]
-     (let [retval#    ~call]
-       (println "  ==> timing:  " ~msg " " (/ (- (System/nanoTime) startTime#) 1000000.0) "ms")
-       retval#)))
+(defn dcm4che_read [filename]
+  (let [stream (DicomInputStream. (File. filename))
+        attr   (.readDataset stream -1 -1)]
+    (pprint (str/split (.toString attr 300 80) #"[\n]"))))
+
+(defn dcm4che_write [filename]
+  (let [attr (Attributes.)]
+    (.setDouble attr Tag/PixelSpacing VR/DS (double-array [1 1]))
+    (.setInt    attr Tag/Rows VR/US (int-array [6400]))
+    (.setInt    attr Tag/Columns VR/US (int-array [200]))
+    (.setInt    attr Tag/HighBit VR/US (int-array [15]))))
 
 ;; (doto (ij.io.FileInfo.)
 ;;   (-> .fileName (set! "x_0.txt"))
 ;;   (-> .directory (set! "resources/PCT")))
 
 ;; (DataInputStream. (FileInputStream. "resources/PCT/x_0.txt"))
-
 
